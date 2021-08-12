408e5dc14658e62fac81b1ec01766ab46534f7ad
ezequielrango <ezequielrango@hotmail.com>
2021-08-12 00:43:26 -0300

    create errors

--- controller/registerController.js
+++ controller/registerController.js
@@ -1,3 +1,4 @@
+const {validationResult} = require('express-validator'); /*Destructuring de los rtdos. de las validaciones, requiriendo Ex-validator*/ 
 
 module.exports = {
     register : (req,res) =>{
@@ -5,6 +6,18 @@ module.exports = {
     },
 
     Processregister : (req,res) =>{
+        const errors= validationResult(req) /* La constante almacena el resultado de las validaciones de lo que viene a través de req. */
+        if(errors.isEmpty()){
+            res.render('register',{
+                usuarioName : req.body.name,
+                color : req.body.theme,
+                email: req.body.email,
+                edad: req.body.age,
+            })
+        }else{
+             res.render('register',{errors : errors.mapped()}) /* Renderizo la vista del formulario junto con los errores. El mapped() convierte el array en un objeto para poder acceder a c/u de sus atributos */
+        }
+       
 
     }
 }
--- validations/register.js
+++ validations/register.js
@@ -2,8 +2,13 @@ const {body}= require('express-validator');
 
 module.exports=[
 
-body('name').notEmpty().withMessage('')
+body('name').notEmpty().withMessage('Debes ingresar un nombre'),
 
+body('theme').notEmpty().withMessage('Debes seleccionar un color'),
 
+body('email').notEmpty().whitMessage('Debes ingresar un email ')
+            .isEmail().whitMessage('Debes ingresar un email válido'),
+
+body('age').isInt() .whitMessage('El valor ingreado debe ser un número'),          
 ]
 

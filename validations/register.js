397ab580a9566ab052ea1fec22ce5048633a2461
juanDeGiorgi <juanmadegiorgi@gmail.com>
2021-08-12 00:48:07 -0300

    fixed errors

--- validations/register.js
+++ validations/register.js
@@ -6,9 +6,9 @@ body('name').notEmpty().withMessage('Debes ingresar un nombre'),
 
 body('theme').notEmpty().withMessage('Debes seleccionar un color'),
 
-body('email').notEmpty().whitMessage('Debes ingresar un email ')
-            .isEmail().whitMessage('Debes ingresar un email válido'),
+body('email').notEmpty().withMessage('Debes ingresar un email ')
+            .isEmail().withMessage('Debes ingresar un email válido'),
 
-body('age').isInt() .whitMessage('El valor ingreado debe ser un número'),          
+body('age').isInt() .withMessage('El valor ingreado debe ser un número'),          
 ]
 
